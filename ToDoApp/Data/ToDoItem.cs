822ebfc8ff1b5697d5fd99df44d288b2cc46307b
Sayani Pathak <sayanipathak01@gmail.com>
2022-01-27 19:35:47 +0000

    Add project files.

---
+++ ToDoApp/Data/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ToDoApp.Data
+{
+    public class ToDoItem
+    {
+        public string Name = "";
+
+        public DateTime DueDate = new DateTime();
+
+        public string WhoseResponsibility = "";
+
+        public string Description = "";
+
+        public DateTime WhenItWasAdded = new DateTime();
+
+        public Guid Id;
+    }
+}
---
+++ ToDoApp/Pages/ToDo.razor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ToDoApp.Data;
+
+namespace ToDoApp.Pages
+{
+    public partial class ToDo
+    {
+        private ToDoItem ToDoItem = new ToDoItem();
+
+        private List<ToDoItem> ToDoItems = new List<ToDoItem>() { 
+            new ToDoItem() { Name = "Sayani's Task", Description ="Learn to code"}, 
+            new ToDoItem() {Name= "Andy's Task", Description = "Teach to code" }
+        };
+
+        public bool IsListVisible { get; set; } = false;
+
+        private void DisplayElement()
+        {
+        //TODO: add a clone method for the ToDoItem.
+            var toDoItem = new ToDoItem() {Name = ToDoItem.Name, Description = ToDoItem.Description, DueDate = ToDoItem.DueDate };
+
+            ToDoItems.Add(toDoItem);
+        }
+
+        public void ToggleList()
+        {
+            //TODO Make this a lambdaw inline.
+            IsListVisible = !IsListVisible;
+        }
+
+    }
+}
