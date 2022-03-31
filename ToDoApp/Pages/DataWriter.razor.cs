c360b8b793a4e60de57dcac59f0c471793da08e3
Sayani Pathak <sayanipathak01@gmail.com>
2022-03-31 19:00:15 +0100

    DataReader added and DataWriter started.

---
+++ ToDoApp/Pages/DataWriter.razor.cs
@@ -0,0 +1,60 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Data;
+
+namespace ToDoApp.Pages
+{
+    public class DataWriter
+    {
+        public void AddToAllItems()
+        {
+            //var data = new[]
+            //{
+            //    new Project { CustomerName = "Olivia", Title = "Mother"},
+            //    new Project { CustomerName = "Lili", Title = "Elder Sister"}
+            //};
+
+            var records = new List<ToDoItem>
+            {
+                new ToDoItem { Name = "Andy's Task", 
+                    //Description = "Teach to code", IsCompleted = true, Id = new Guid(),
+                    //DueDate = new DateTime(), WhenItWasAdded = DateTime.Now, WhoseResponsibility = "rtyuio"
+                    }
+            };
+
+            using (var writer = new StreamWriter(@"C:/Users/Sayani Pathak/source/repos/ToDoApp/ToDoApp/Data/AllItems.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+
+        public void AddToAllItems2()
+        {
+            var records = new List<ToDoItem>
+            {
+                new ToDoItem { Id = new Guid(), Name = "one" },
+            };
+            
+            using (var writer = new StreamWriter(@"C:/Users/Sayani Pathak/source/repos/ToDoApp/ToDoApp/Data/AllItems - Copy.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+
+
+        }
+
+        public class Foo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ ToDoAppTests/Pages/DataWriterTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ToDoApp.Pages;
+
+namespace ToDoAppTests.Pages
+{
+    [TestClass()]
+    public class DataWriterTests
+    {
+        [TestMethod()]
+        public void WriteCorrectDataTest()
+        {
+            var dataWriter = new DataWriter();
+            dataWriter.AddToAllItems2();
+        }
+              
+    }
+}
